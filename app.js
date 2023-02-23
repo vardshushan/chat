e2ade2435c118c9d77f2d99b1f8f74beeaa188de
Shushan Vardanyan <shushanvardanian97@gmail.com>
2023-02-23 12:25:11 +0400

    real-time chat with socket.io

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+let name;
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('joining msg', (username) => {
+        name = username;
+        io.emit('chat message', `${name} joined the chat.`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('chat message', `---${name} left the chat---`);
+
+    });
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on :3000');
+});
+
+
